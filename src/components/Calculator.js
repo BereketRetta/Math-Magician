d1e0dba5eaafceac11ffc231889e222984847d79
Bereket Retta <bereket@etmsoftwareplc.com>
2022-02-22 21:30:49 +0300

    remove eslint disable line

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './calculator.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   render() {
     return (
